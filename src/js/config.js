53b261386232031099ae57ccc229f2e42084b101
Айдос <aydosizimbetov@icloud.com>
2022-05-19 15:59:09 +0500

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,243 @@
+$('.header-slide-block').slick({
+    infite:true,
+    slideToScroll:1,
+    SlideShow:1,
+    autoplay:false,
+    speed:1500,
+    arrows:false,
+    dots:true,
+})
+
+$('.sushi-block').slick({
+    infite:true,
+    slideToScroll:1,
+    SlideShow:1,
+    speed:1500,
+    arrows:false,
+    centerPadding:"35px",
+    centerMode: true,
+
+    responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            speed:1500,
+          }
+        },
+        {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              speed:1500,
+              centerMode:false,
+              centerPadding:0,
+              dots:true,
+            }
+        },
+        {
+            breakpoint: 3000,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              speed:1500,
+              centerMode:false,
+              centerPadding:0,
+              dots:true,
+            }
+        }
+    ]
+})
+
+$('.rol-block').slick({
+    infite:true,
+    slideToScroll:1,
+    SlideShow:1,
+    speed:1500,
+    arrows:false,
+    // dots:true,  
+    centerPadding:"35px",
+    centerMode: true,
+
+    responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            speed:1500,
+          }
+        },
+        {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              speed:1500,
+              centerMode:false,
+              centerPadding:0,
+              dots:true,
+            }
+        },
+        {
+            breakpoint:3000,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              speed:1500,
+              centerMode:false,
+              centerPadding:0,
+              dots:true,
+            }
+        }
+    ]
+})
+
+$('.pizza-block').slick({
+  infite:true,
+  slideToScroll:1,
+  SlideShow:1,
+  speed:1500,
+  arrows:false,
+  // dots:true,  
+  centerPadding:"35px",
+  centerMode: true,
+
+  responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          speed:1500,
+        }
+      },
+      {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      },
+      {
+          breakpoint: 3000,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      }
+  ]
+})
+
+$('.wok-block').slick({
+  infite:true,
+  slideToScroll:1,
+  SlideShow:1,
+  speed:1500,
+  arrows:false,
+  // dots:true,  
+  centerPadding:"35px",
+  centerMode: true,
+
+  responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          speed:1500,
+        }
+      },
+      {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      },
+      {
+          breakpoint: 3000,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      }
+  ]
+})
+
+$('.new-block').slick({
+  infite:true,
+  slideToScroll:1,
+  SlideShow:1,
+  speed:1500,
+  arrows:false,
+  // dots:true,  
+  centerPadding:"35px",
+  centerMode: true,
+
+  responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          speed:1500,
+        }
+      },
+      {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      },
+      {
+          breakpoint: 3000,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            speed:1500,
+            centerMode:false,
+            centerPadding:0,
+            dots:true,
+          }
+      }
+  ]
+})
